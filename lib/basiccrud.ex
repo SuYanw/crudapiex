a6307e9352f86e504c095bfa092cd67a0c239077
glaubert <suyann12@hotmail.com>
2022-06-15 10:15:49 -0300

    finish user CRUD

--- lib/basiccrud.ex
+++ lib/basiccrud.ex
@@ -1,9 +1,8 @@
 defmodule Basiccrud do
-  @moduledoc """
-  Basiccrud keeps the contexts that define your domain
-  and business logic.
 
-  Contexts are also responsible for managing your data, regardless
-  if it comes from the database, an external API or others.
-  """
+  alias Basiccrud.User
+
+  defdelegate create_user(params), to: User.Create, as: :call
+  defdelegate delete_user(params), to: User.Delete, as: :call
+  defdelegate update_user(params), to: User.Update, as: :call
 end
