b9ca63259a61fdd6617f019a94cb101bf8f4452f
glaubert <suyann12@hotmail.com>
2022-06-15 10:33:01 -0300

    added get_user function

--- lib/basiccrud.ex
+++ lib/basiccrud.ex
@@ -5,4 +5,5 @@ defmodule Basiccrud do
   defdelegate create_user(params), to: User.Create, as: :call
   defdelegate delete_user(params), to: User.Delete, as: :call
   defdelegate update_user(params), to: User.Update, as: :call
+  defdelegate get_user(params), to: User.Get, as: :call
 end
---
+++ lib/basiccrud/user/get.ex
@@ -0,0 +1,18 @@
+defmodule Basiccrud.User.Get do
+    
+    def call(%{"id" => uuid} = _params) do
+        case Ecto.UUID.cast(uuid) do
+            :error -> {:error, "Invalid User ID Format!"}
+            {:ok, uuid} -> validate_before_fetch(uuid)
+        end
+    end
+
+    def validate_before_fetch(uuid) do
+        case Basiccrud.Repo.get(Basiccrud.User, uuid) do
+            nil -> {:error, "User not found"}
+            _ -> fetch_user(uuid) 
+        end
+    end
+
+    defp fetch_user(uuid), do: Basiccrud.Repo.get(Basiccrud.User, uuid)
+end
