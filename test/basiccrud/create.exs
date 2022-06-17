2aadaf69f2aaecdf531c4c4f44e54aecfbba3c63
glaubert <suyann12@hotmail.com>
2022-06-17 12:22:03 -0300

    append create test

--- test/basiccrud/create.exs
+++ test/basiccrud/create.exs
@@ -1,5 +1,26 @@
 defmodule Basiccrud.User.CreateTest do
     use Basiccrud.DataCase
 
-    
+    describe "call/1" do
+        test "when all params are valid, create a user" do
+            
+            params = %{name: "Glaubert", password: "123456", age: 30, email: "glaubert@domain.com.br"}
+            reply =
+                params 
+                |> Basiccrud.User.Create.call()
+
+            assert {:ok, %Basiccrud.User{name: "Glaubert", password: "123456"}} = reply
+        end
+
+        test "when any param ins invalid, returns error" do
+            params = %{name: "Glaubert", password: "123456", age: 30, email: 22}
+            reply =
+                params 
+                |> Basiccrud.User.Create.call()
+
+            assert {:error, changeset} = reply
+            assert errors_on(changeset) == %{email: ["is invalid"]}   
+        end
+        
+    end
 end
