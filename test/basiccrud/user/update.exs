d87df7ff2b9fef764989cd763f245f94102626d3
glaubert <suyann12@hotmail.com>
2022-06-17 16:46:14 -0300

    created uptade.exs

--- test/basiccrud/user/update.exs
+++ test/basiccrud/user/update.exs
@@ -8,17 +8,32 @@ defmodule Basiccrud.User.UpdateTest do
     
 
     describe "call/1" do
+
         test "when all params are valid, update a user" do
             test_user_params = %{name: "Glaubert", password: "123456", age: 30, email: "glaubert@domain.com.br"}
-
-            params = %{name: "Glaubert", password: "123456", age: 30, email: "glaubert@domain.com.br"}
-            reply =
-                params 
-                |> Basiccrud.User.Create.call()
             
-            {:ok, %Basiccrud.User.user} = reply
+            {:ok, user} = Basiccrud.User.Create.call(test_user_params)
+            {:ok, uuid} = Map.fetch(user, :id) 
 
-            assert "1234" = %Basiccrud.User.user.id
+            new_user_params = %{"id" => uuid, "name" => "newname"}
+
+            response = Basiccrud.update_user(new_user_params)
+
+            assert {:ok, %Basiccrud.User{name: "newname"}} = response
+
+            #delete user created
+            uuid
+            |> Basiccrud.delete_user()
         end
+
+        test "when has no valid uuid, return errors" do
+
+            new_user_params = %{"id" => "invalid_uuid", "name" => "newname"}
+
+            response = Basiccrud.update_user(new_user_params)
+
+            assert {:error, "Invalid User ID Format!"} = response
+        end
+
     end
 end
